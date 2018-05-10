2f0cf1a638f4bd425db8468ebaaa48ae233b6a1d
Nathaniel SEYLER <nathaniel.seyler@etu.unistra.fr>
2018-05-10 19:22:36 +0200

    ajout du rebind RMI

--- src/main/java/BlockNode.java
+++ src/main/java/BlockNode.java
@@ -1,4 +1,20 @@
+import java.net.* ;
+import java.rmi.* ;
+
 public class BlockNode {
 	//class qui rebind RMI
+	//private LinkedList<String> fileOperations;
+	//private LinkedList<Participant> participants;
 	//TODO implements here
+	public static void main(String [] args)
+	{
+		try
+			{
+				NodeImpl objLocal = new NodeImpl () ;
+				Naming.rebind("Node",objLocal) ;
+				System.out.println("Serveur Node pret") ;
+			}
+		catch (RemoteException re) { System.out.println(re) ; }
+		catch (MalformedURLException e) { System.out.println(e) ; }
+	}
 }
